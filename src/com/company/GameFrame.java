7078f7280ca737848cf6916ef9ec6bf12e61d4e9
shivambadoni19992 <shivambadoni.39@gmail.com>
2022-08-29 20:31:30 +0530

    Initial commit

---
+++ src/com/company/GameFrame.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+
+    GamePanel panel;
+
+    GameFrame()
+    {
+        panel=new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        AudioPlayerExample1 a=new AudioPlayerExample1();
+        a.play();
+
+    }
+
+
+}
